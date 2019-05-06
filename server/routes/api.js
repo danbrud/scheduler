8548ea0be6123dd1bdf00f105f06f90a05487ddc
Danny <dannybrudner@gmail.com>
2019-05-06 06:31:23 -0400

    Setup server file

---
+++ server/routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const request = require('request')
+const router = express.Router()
+const mongoose = require('mongoose')
+
+
+router.get('/sanity', function (req, res) {
+    res.send('OK!')
+})
+
+
+module.exports = router
